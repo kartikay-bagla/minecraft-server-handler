28c6a80a30311dc7101ef1958e4d314f0aebb847
kartikay-bagla <kartikaybagla@gmail.com>
2023-08-14 21:32:57 +0530

    fix: fixed factorio command

--- factorio_utils.py
+++ factorio_utils.py
@@ -4,5 +4,5 @@ from rcon.source import Client
 def get_info(host: str, port: int, password: str) -> dict:
     client = Client(host, port, passwd=password)
     client.connect(login=True)
-    response = client.run("/players", "only", "count")
+    response = client.run("/players", "online", "count")
     return {"player_count": int(response.strip("Players (").strip("):\n"))}
