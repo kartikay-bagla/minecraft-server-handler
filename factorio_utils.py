6f80e6b6ebb39f177f2a2bb8d297f3ead4defa2c
kartikay-bagla <kartikaybagla@gmail.com>
2023-08-14 21:36:19 +0530

    fix: fixed factorio command stripping

--- factorio_utils.py
+++ factorio_utils.py
@@ -5,4 +5,4 @@ def get_info(host: str, port: int, password: str) -> dict:
     client = Client(host, port, passwd=password)
     client.connect(login=True)
     response = client.run("/players", "online", "count")
-    return {"player_count": int(response.strip("Players (").strip("):\n"))}
+    return {"player_count": int(response.strip("Online Players (").strip("):\n"))}
